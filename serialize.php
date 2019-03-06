854a11b0d7f32a16cc015dc4d93d64021d78869d
Anatoliy Marchenko <toxerius7@gmail.com>
2019-03-06 20:03:48 +0200

    fix

--- serialize.php
+++ serialize.php
@@ -3,6 +3,8 @@ include_once 'abstract.php';
 
 $object = file_get_contents('data.txt');
 $figure = unserialize($object);
-echo substr(get_class($figure), strrpos('/', get_class($figure)));
+echo substr(get_class($figure), strrpos('\\', get_class($figure)));
+
+
 
 $figure->Show();
